06229f727c51168ecc49c3f053b2ec8566d80b44
Victor Akinnibosun <victor.akinnibosun23@gmail.com>
2023-08-14 06:34:13 +0100

    Exercise 33 done.

---
+++ ex33.py
@@ -0,0 +1,17 @@
+numbers = []
+
+def iterate(iteration, inc):
+    #i = 0
+    for i in range(0, iteration, inc):
+        print(f"At the top i is {i}")
+        numbers.append(i)
+
+        #i = i + inc
+        print("Numbers now:", numbers)
+        print(f"At the bottom i is {i}")
+
+iterate(15, 3)
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
