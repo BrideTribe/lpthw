7226c9c599f4882ca84d1299ec8c5aae29a527c3
Victor Akinnibosun <victor.akinnibosun23@gmail.com>
2023-08-15 05:59:43 +0100

    complete exercise 34

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python3.10', 'peacock', 'kangaroo', 'whale', 'patypus']
+
+print(f"The animal at 1 is {animals[0]}")
+print(f"The third (3rd) animal is {animals[2]}")
+print(f"The first (1st) animal: {animals[0]}")
+print(f"The animal at 3 is {animals[2]}")
+print(f"The fifth (5th) animal is {animals[4]}")
+print(f"The animal at 2 is {animals[1]}")
+print(f"The sixth (6th) animal is {animals[-1]}")
+print(f"The element at 4 is {animals[3]}")
